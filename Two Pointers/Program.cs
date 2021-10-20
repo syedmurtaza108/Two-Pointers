8b0da6d4e4c46b89c7b1c96c8b9434af8ac69b75
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-20 14:09:13 +0500

    Add project files.

---
+++ Two Pointers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Two_Pointers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasTwoPairsEqualsTarget(new int[] { 1,2,3,4,5,6 }, 6));
+        }
+        static bool HasTwoPairsEqualsTarget(int[] array, int target)
+        {
+            Array.Sort(array);
+            int pointer1 = 0, pointer2 = array.Length - 1;
+            while (pointer1 < pointer2)
+            {
+                var sum = array[pointer1] + array[pointer2];
+                if (sum == target) return true;
+
+                if (sum < target)
+                    pointer1++;
+                else
+                    pointer2--;
+            }
+
+            return false;
+        }
+    }
+}
